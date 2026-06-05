31f414ad4052a68415223447e5c958bd403c8eb9
zoya-chizhova <chizhovazoya06@gmail.com>
2026-06-05 13:56:34 +0300

    Update Directory.h

--- Directory.h
+++ Directory.h
@@ -1,24 +1,20 @@
 #ifndef DIRECTORY_H
 #define DIRECTORY_H
-
 #include "Entry.h"
 #include <vector>
 #include <memory>
 #include <string>
-
 class Directory : public Entry {
 private:
-    std::vector<std::unique_ptr<Entry>> children;
-
+std::vector<std::unique_ptr<Entry>> children;
 public:
-    Directory(const std::string& n);
-    Entry* asDirectory() override;
-    void displayContent() const override;
-    bool edit(const std::string&) override;
-    Entry* findChild(const std::string& name);
-    void addChild(std::unique_ptr<Entry> child);
-    const std::vector<std::unique_ptr<Entry>>& getChildren() const;
-    void removeChild(const std::string& entryName);
+Directory(const std::string& n);
+Entry* asDirectory() override;
+void displayContent() const override;
+bool edit(const std::string&) override;
+Entry* findChild(const std::string& name);
+void addChild(std::unique_ptr<Entry> child);
+const std::vector<std::unique_ptr<Entry>>& getChildren() const;
+void removeChild(const std::string& entryName);
 };
-
 #endif
