cde8582f9bbffa99aa11d47cfa3560b632861723
zoya-chizhova <chizhovazoya06@gmail.com>
2026-06-05 18:39:48 +0300

    Update Directory.cpp

--- Directory.cpp
+++ Directory.cpp
@@ -3,40 +3,40 @@
 #include <algorithm>
 Directory::Directory(const std::string& n) : Entry(n) {}
 Entry* Directory::asDirectory() {
-return this;
+  return this;
 }
 void Directory::displayContent() const {
-for (const auto& child : children) {
-if (!child->isHidden()) {
-std::cout << child->getDisplayName() << "\n";
-}
-}
+  for (const auto& child : children) {
+    if (!child->isHidden()) {
+      std::cout << child->getDisplayName() << "\n";
+    }
+  }
 }
 bool Directory::edit(const std::string&) {
-std::cout << "Cannot edit directory\n";
-return false;
+  std::cout << "Cannot edit directory\n";
+  return false;
 }
 Entry* Directory::findChild(const std::string& name) {
-auto it = std::find_if(children.begin(), children.end(),
-[&name](const std::unique_ptr<Entry>& e) {
-return e->getDisplayName() == name;
-});
-return (it != children.end()) ? it->get() : nullptr;
+  auto it = std::find_if(children.begin(), children.end(),
+  [&name](const std::unique_ptr<Entry>& e) {
+    return e->getDisplayName() == name;
+  });
+  return (it != children.end()) ? it->get() : nullptr;
 }
 void Directory::addChild(std::unique_ptr<Entry> child) {
-children.push_back(std::move(child));
+  children.push_back(std::move(child));
 }
 const std::vector<std::unique_ptr<Entry>>& Directory::getChildren() const {
-return children;
+  return children;
 }
 void Directory::removeChild(const std::string& entryName) {
-auto it = std::find_if(children.begin(), children.end(),
-[&entryName](const std::unique_ptr<Entry>& e) {
-return e->getDisplayName() == entryName;
+  auto it = std::find_if(children.begin(), children.end(),
+  [&entryName](const std::unique_ptr<Entry>& e) {
+    return e->getDisplayName() == entryName;
 });
-if (it != children.end()) {
-children.erase(it);
-} else {
-std::cout << "Entry not found: " << entryName << "\n";
-}
+  if (it != children.end()) {
+    children.erase(it);
+  } else {
+    std::cout << "Entry not found: " << entryName << "\n";
+  }
 }
